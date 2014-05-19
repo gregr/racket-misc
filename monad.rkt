d084deb2154b0d7a6384a1e34643221eb26c435f
gregr <greg.weiqi@gmail.com>
2014-05-18 22:45:38 -0400

    Add monad.rkt

---
+++ monad.rkt
@@ -0,0 +1,66 @@
+#lang racket/base
+(provide
+  (struct-out monad)
+  pure
+  bind
+  identity-monad
+  with-monad
+  let1/monad
+  let/monad
+  let/with-monad
+  begin/monad
+  begin/with-monad
+  )
+
+(require (for-syntax racket/base))
+(require "match.rkt")
+(require "record.rkt")
+(require racket/function)
+(require racket/match)
+
+(record monad pure bind)
+
+(define identity-monad (monad identity (lambda (prev next) (next prev))))
+
+(define current-monad (make-parameter identity-monad))
+
+(define (pure value) ((monad-pure (current-monad)) value))
+(define (bind prev next) ((monad-bind (current-monad)) prev next))
+
+(define-syntax with-monad
+  (syntax-rules ()
+    ((_ monad body ...)
+     (parameterize ((current-monad monad)) body ...))))
+
+(define-syntax let1/monad
+  (syntax-rules (<- =)
+    ((_ (pattern <- value) body ...)
+     (bind value (lambda/destruct (pattern) body ...)))
+    ((_ (pattern = value) body ...)
+     (match-let ((pattern value)) body ...))))
+
+(define-syntax (let/monad stx)
+  (syntax-case stx ()
+    ((_ (binding ...) body ...)
+     (for/fold ((body #'(begin body ...)))
+               ((binding (reverse (syntax->list #'(binding ...)))))
+      #`(let1/monad #,binding #,body)))))
+
+(define-syntax let/with-monad
+  (syntax-rules ()
+    ((_ monad bindings body ...)
+     (with-monad monad
+      (let/monad bindings body ...)))))
+
+(define-syntax begin/monad
+  (syntax-rules ()
+    ((_ pattern bind-sym value body ...)
+     (let1/monad (pattern bind-sym value) (begin/monad body ...)))
+    ((_ body)
+     body)))
+
+(define-syntax begin/with-monad
+  (syntax-rules ()
+    ((_ monad body ...)
+     (with-monad monad
+      (begin/monad body ...)))))
