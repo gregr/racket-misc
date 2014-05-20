2c2769a735e895978328943c8dfc43fd0ccaab1e
gregr <greg.weiqi@gmail.com>
2014-05-19 23:13:10 -0400

    Define catamorphic matching

--- match.rkt
+++ match.rkt
@@ -2,9 +2,14 @@
 (provide
   lambda/destruct
   define/destruct
+  cata
+  lambda/cata
+  define/cata
+  match/cata
   )
 
 (require (for-syntax racket/base))
+(require racket/function)
 (require racket/match)
 
 (define-syntax (lambda/destruct stx)
@@ -21,3 +26,33 @@
     ((_ (name pattern ...) body ...)
      (define name
        (lambda/destruct (pattern ...) body ...)))))
+
+(define current-cata
+  (make-parameter
+    (lambda args (error "use of current-cata not in a catamorphism" args))))
+
+(define-match-expander cata
+  (lambda (stx)
+    (syntax-case stx ()
+      ((_ pats ...)
+       #'(app (current-cata) pats ...)))))
+
+(define-syntax (lambda/cata stx)
+  (syntax-case stx ()
+    ((_ (pattern body ...) ...)
+     (with-syntax (((cata-name) (generate-temporaries #'(cata-name))))
+      #'((thunk
+           (define (cata-name value)
+             (parameterize ((current-cata cata-name))
+              (match value (pattern body ...) ...)))
+           cata-name))))))
+
+(define-syntax define/cata
+  (syntax-rules ()
+    ((_ name body ...)
+     (define name (lambda/cata body ...)))))
+
+(define-syntax match/cata
+  (syntax-rules ()
+    ((_ value body ...)
+     ((lambda/cata body ...) value))))
