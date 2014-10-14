d2901aa86dce1e67b0eff8d157070be24e6f750a
gregr <greg.weiqi@gmail.com>
2014-10-13 22:03:47 -0400

    Define with-streams, fetch-from, fetch-from-try

--- stream.rkt
+++ stream.rkt
@@ -3,12 +3,16 @@
   fetch
   fetch-try
   with-stream
+  fetch-from
+  fetch-from-try
+  with-streams
   )
 
 (require
   "state.rkt"
   "maybe.rkt"
   "monad.rkt"
+  racket/dict
   racket/match
   racket/stream
   )
@@ -58,4 +62,51 @@
       (nothing) <- fetch-try
       (pure (list v0 v1)))
     (list 1 2)
+    ))
+
+(define (fetch-from-try name)
+  (begin/monad
+    streams <- get
+    stream = (dict-ref streams name)
+    (if (stream-empty? stream)
+      (pure (nothing))
+      (begin/monad
+        first = (stream-first stream)
+        rest = (stream-rest stream)
+        streams = (dict-set streams name rest)
+        _ <- (put streams)
+        (pure (just first))))))
+(define (fetch-from name)
+  (begin/monad
+    maybe-val <- (fetch-from-try name)
+    (match maybe-val
+      ((nothing) (error (format "nothing to fetch from stream '~a'" name)))
+      ((just val) (pure val)))))
+
+(define-syntax with-streams
+  (syntax-rules ()
+    ((_ ((name stream) ...) body ...)
+     (let ((streams (make-immutable-hash (list (cons 'name stream) ...))))
+       (state-eval (begin/with-monad state-monad body ...) streams)))))
+
+(module+ test
+  (check-equal?
+    (with-streams ((one (gen->stream (generator _ (yield 1) (yield 2))))
+                   (two (gen->stream (generator _ (yield 3) (yield 4)))))
+      v0 <- (fetch-from 'one)
+      v1 <- (fetch-from 'two)
+      (pure (list v0 v1)))
+    (list 1 3)
+    ))
+
+(module+ test
+  (check-equal?
+    (with-streams ((one (gen->stream (generator _ (yield 1) (yield 2))))
+                   (two (gen->stream (generator _ (yield 3) (yield 4)))))
+      (just v0) <- (fetch-from-try 'one)
+      v1 <- (fetch-from 'two)
+      v2 <- (fetch-from 'two)
+      (nothing) <- (fetch-from-try 'two)
+      (pure (list v0 v1 v2)))
+    (list 1 3 4)
     ))
