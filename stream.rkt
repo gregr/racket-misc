f9e7b85fc5676137aa858a6486c2c27bd9d137dd
gregr <greg.weiqi@gmail.com>
2014-10-16 20:59:02 -0400

    Define stream-state-monad

--- stream.rkt
+++ stream.rkt
@@ -1,11 +1,23 @@
 #lang racket/base
 (provide
   stream-monad
+  stream-state-monad
+  stream-state-run
+  peek
+  peek-try
+  pop
+  pop-try
+  push
+  with-stream
   )
 
 (require
+  "state.rkt"
+  "maybe.rkt"
   "match.rkt"
   "monad.rkt"
+  racket/function
+  racket/match
   racket/stream
   )
 
@@ -45,6 +57,70 @@
     (list)
     ))
 
+(define stream-state-monad (state-monad-t maybe-monad))
+(define stream-state-run state-t-run)
+(define (stream->maybe stream)
+  (if (stream-empty? stream) (nothing) (just (stream-first stream))))
 
+(define peek-try
+  (begin/with-monad stream-state-monad
+    stream <- get
+    (pure (stream->maybe stream))))
+(define peek
+  (begin/with-monad stream-state-monad
+    maybe <- peek-try
+    (lift (thunk maybe))))
+(define pop-try
+  (begin/with-monad stream-state-monad
+    stream <- get
+    maybe = (stream->maybe stream)
+    _ <- (put (if (stream-empty? stream) stream (stream-rest stream)))
+    (pure maybe)))
+(define pop
+  (begin/with-monad stream-state-monad
+    maybe <- pop-try
+    (lift (thunk maybe))))
+(define (push val)
+  (begin/with-monad stream-state-monad
+    stream <- get
+    (put (stream-cons val stream))))
 
+(define-syntax with-stream
+  (syntax-rules ()
+    ((_ stream body ...)
+     ((stream-state-run (begin/with-monad stream-state-monad body ...)) stream))))
 
+(module+ test
+  (check-equal?
+    (with-stream (list 1 2)
+      v0 <- pop
+      v1 <- pop
+      (pure (list v1 v0)))
+    (just (cons (list 2 1) '()))
+    ))
+
+(module+ test
+  (check-equal?
+    (with-stream (list 1 2)
+      (just v0) <- pop-try
+      v1 <- pop
+      (nothing) <- pop-try
+      (pure (list v1 v0)))
+    (just (cons (list 2 1) '()))
+    ))
+
+(module+ test
+  (check-equal?
+    (match-let (((just (cons result stream))
+        (with-stream (list 1 2)
+          v0 <- peek
+          v1 <- pop
+          v2 <- peek
+          v3 <- pop
+          (nothing) <- peek-try
+          _ <- (push 5)
+          v4 <- pop
+          (pure (list v0 v1 v2 v3 v4)))))
+      (just (cons result (stream->list stream))))
+    (just (cons (list 1 1 2 2 5) '()))
+    ))
