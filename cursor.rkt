74c757d90715cfba321f22cfd7ca28e0045c6d30
gregr <greg.weiqi@gmail.com>
2015-02-13 15:59:44 -0500

    Define default-filling cursor and lense operators

--- cursor.rkt
+++ cursor.rkt
@@ -20,6 +20,16 @@
   ::.*  ; like ::. but take each path component as an additional argument
   ::=*  ; like ::= but take each path component as an additional argument
   ::~*  ; like ::~ but take each path component as an additional argument
+  ; like the above operators, but fill in empty paths during traversal using
+  ; a constructor (which takes current focus and missing key as arguments)
+  ::%@
+  ::%.
+  ::%=
+  ::%~
+  ::%@*
+  ::%.*
+  ::%=*
+  ::%~*
 
   ;; lens operators
   :o  ; merge a list of paths into one path
@@ -30,6 +40,14 @@
   :.* ; like :. but the arguments taken are segments of a single path
   :=* ; like := but the arguments taken are segments of a single path
   :~* ; like :~ but the arguments taken are segments of a single path
+  ; like the above operators, but fill in empty paths during traversal using
+  ; a constructor (which takes current focus and missing key as arguments)
+  :%.
+  :%=
+  :%~
+  :%.*
+  :%=*
+  :%~*
   )
 
 (require
@@ -37,6 +55,7 @@
   "function.rkt"
   "list.rkt"
   "record.rkt"
+  "sugar.rkt"
   racket/dict
   racket/function
   racket/match
@@ -76,8 +95,16 @@
      (match-let* ((`(,p-ref ,_) (ref+set focus))
                   (new-focus (p-ref focus key)))
        (cursor new-focus (cons key keys) (cons focus ancestors))))))
+(def (cursor-descend% new cur key)
+  focus = (cursor-focus cur)
+  `(,_ ,p-set) = (ref+set focus)
+  cur = (if (datum-has-key? focus key)
+          cur (cursor-refocus cur (p-set focus key (new focus key))))
+  (cursor-descend cur key))
 (define (cursor-descend* cur keys)
   (foldl (flip cursor-descend) cur keys))
+(define (cursor-descend%* new cur keys)
+  (foldl (flip (curry cursor-descend% new)) cur keys))
 (define (cursor-descend*/either cur keys)
   (match keys
     ('()             (right cur))
@@ -100,12 +127,22 @@
 
 (define (::@ cur path) (cursor-descend* cur path))
 (define (::@? cur path) (cursor-descend*/either cur path))
+(define (::%@ new cur path) (cursor-descend%* new cur path))
 (define (::. cur path) (cursor-focus (::@ cur path)))
+(define (::%. new cur path) (cursor-focus (::%@ new cur path)))
 (define (::= cur val path)
   (cursor-ascend-to (cursor-refocus (::@ cur path) val)
                     cur))
+(define (::%= new cur val path)
+  (cursor-ascend-to (cursor-refocus (::%@ new cur path) val)
+                    cur))
 (define (::~ cur trans path)
   (let ((cur-next (::@ cur path)))
+    (cursor-ascend-to
+      (cursor-refocus cur-next (trans (cursor-focus cur-next)))
+      cur)))
+(define (::%~ new cur trans path)
+  (let ((cur-next (::%@ new cur path)))
     (cursor-ascend-to
       (cursor-refocus cur-next (trans (cursor-focus cur-next)))
       cur)))
@@ -116,12 +153,24 @@
 (define (::=* cur val . path)   (::= cur val path))
 (define (::~* cur trans . path) (::~ cur trans path))
 
+(define (::%@* new cur . path)       (::%@ new cur path))
+(define (::%.* new cur . path)       (::%. new cur path))
+(define (::%=* new cur val . path)   (::%= new cur val path))
+(define (::%~* new cur trans . path) (::%~ new cur trans path))
+
 (define (:. src path)          (::. (::0 src) path))
 (define (:= src val path)      (::.* (::= (::0 src) val path)))
 (define (:~ src trans path)    (::.* (::~ (::0 src) trans path)))
 (define (:.* src . path)       (:. src path))
 (define (:=* src val . path)   (:= src val path))
 (define (:~* src trans . path) (:~ src trans path))
+
+(define (:%. new src path)          (::%. new (::0 src) path))
+(define (:%= new src val path)      (::.* (::%= new (::0 src) val path)))
+(define (:%~ new src trans path)    (::.* (::%~ new (::0 src) trans path)))
+(define (:%.* new src . path)       (:%. new src path))
+(define (:%=* new src val . path)   (:%= new src val path))
+(define (:%~* new src trans . path) (:%~ new src trans path))
 
 (module+ test
   (record foo x y)
@@ -134,4 +183,10 @@
   (check-equal? (:.* (:~* foobar (curry + 3) 'x 'a) 'x 'a)
                 8)
   (check-equal? (:=* 'src 'tgt) 'tgt)
+  (check-equal?
+    (:%=* (lambda _ (hash 'default 0))
+          (hash 'a 1 'b (hash 'c 3)) 5
+          'b 'd 'e)
+    (hash 'a 1 'b (hash 'c 3 'd (hash 'default 0 'e 5)))
+    )
   )
