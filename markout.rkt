1eb7402948d8ac2a180815b19d66ada47ad90279
gregr <greg.weiqi@gmail.com>
2015-02-18 14:02:28 -0500

    Fix separated

--- markout.rkt
+++ markout.rkt
@@ -177,7 +177,7 @@
       init-items = (forl
                      item <- init-items
                      (tight-pair style item separator))
-      (append init-items last-item))))
+      (append init-items (list last-item)))))
 (def (bracketed-chain prefix suffix attr outer-style inner-style items)
   elements = (doc-chain inner-style attr items)
   suffix-chain = (doc-chain outer-style attr-tight-aligned (list elements suffix))
