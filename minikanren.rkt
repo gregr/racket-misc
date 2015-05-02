288dec6b99d85598154d23e1b2384713600180ff
gregr <greg.weiqi@gmail.com>
2015-05-02 19:11:48 -0400

    Define matche

--- minikanren.rkt
+++ minikanren.rkt
@@ -2,6 +2,7 @@
 (provide
   conde
   exist
+  matche
   run
   run*
   )
@@ -9,7 +10,10 @@
 (require
   "microkanren.rkt"
   racket/list
-  )
+  (for-syntax
+    racket/base
+    racket/list
+    ))
 
 (module+ test
   (require rackunit))
@@ -30,9 +34,32 @@
      (muk-reify muk-var->symbol
                 (map muk-var (range (length '(xs ...))))
                 (muk-take n ((exist (xs ...) gs ...) muk-state-empty))))))
-
 (define-syntax run*
   (syntax-rules () ((_ body ...) (run -1 body ...))))
+
+(define-for-syntax (pattern->identifiers pat)
+  (define (unquote-pats stx)
+    (syntax-case stx (unquote)
+      ((unquote pat) (list #'pat))
+      ((head . tail) (append (unquote-pats #'head) (unquote-pats #'tail)))
+      (_ '())))
+  (if (identifier? pat) (list pat)
+    (syntax-case pat (quote quasiquote)
+      ((quote _) '())
+      ((quasiquote stx)
+       (append* (map pattern->identifiers (unquote-pats #'stx))))
+      ((_ args ...)
+       (append* (map pattern->identifiers (syntax->list #'(args ...)))))
+      (_ '()))))
+(define-syntax (match1e stx)
+  (syntax-case stx ()
+    ((_ arg pattern gs ...)
+     (let ((vnames (pattern->identifiers #'pattern)))
+       #`(exist (#,@vnames) (== pattern arg) gs ...)))))
+(define-syntax matche
+  (syntax-rules ()
+    ((_ arg (pattern gs ...) ...)
+     (let ((param arg)) (disj* (match1e param pattern gs ...) ...)))))
 
 (module+ test
   (check-equal?
@@ -41,15 +68,12 @@
   (check-equal?
     (run* (x y) (== (cons (list x 3) 5) (list (list 4 y) 5)))
     '())
-  (define (appendo l r lr)
-    (conde
-      ((== '() l) (== r lr))
-      ((exist (lfirst lrest lrestr)
-        (== (cons lfirst lrest) l)
-        (== (cons lfirst lrestr) lr)
-        (appendo lrest r lrestr)
-        ;(== (list lrest r) lrestr)
-        ))))
+  (define (appendo ls rs lsrs)
+    (matche ls
+      ('() (== rs lsrs))
+      ((cons l0 ms) (exist (msrs)
+                      (== (cons l0 msrs) lsrs)
+                      (appendo ms rs msrs)))))
   (check-equal?
     (run* (x y) (appendo x y (range 1 6)))
     '((() (1 2 3 4 5))
