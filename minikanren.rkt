c35fd003fea22a46d6863bec25df7b7e27887641
gregr <greg.weiqi@gmail.com>
2015-05-07 20:14:05 -0400

    Add more tests (such as send-more-moneyo)

--- minikanren.rkt
+++ minikanren.rkt
@@ -80,13 +80,29 @@
                      (cons vr val))
        (if (null? or-diseqs) #f (muk-func-app '=/= or-diseqs))))))
 
+(define (interp-+ a b)
+  (if (or (muk-term? a) (muk-term? b)) (muk-func-app '+ (list a b))
+    (+ a b)))
+(define (interp-< a b)
+  (if (or (muk-term? a) (muk-term? b)) (muk-func-app '< (list a b))
+    (< a b)))
+
 (define interpretations
   (hash
     '=/= interp-=/=
+    '+ interp-+
+    '< interp-<
     ))
 
 (define with-constraints (interpret interpretations))
 
+(define (ino domain . xs)
+  (forf goal = (conj*)
+        x <- xs
+        (conj goal
+              (forf goal = (disj*)
+                    el <- domain
+                    (disj goal (== x el))))))
 (define (=/= e0 e1) (== #t (muk-func-app '=/= (list (cons e0 e1)))))
 (define (all-diffo xs)
   (matche xs
@@ -96,7 +112,11 @@
       (=/= a ad)
       (all-diffo `(,a . ,dd))
       (all-diffo `(,ad . ,dd)))))
-
+(define (+o a b a+b)
+  (== (muk-func-app '+ (list a b)) a+b))
+(define (<o a b)
+  (== (muk-func-app '< (list a b)) #t))
+(define (<=o a b) (conde ((== a b)) ((<o a b))))
 (module+ test
   (check-equal?
     (run* (x y) (== (cons (list x 3) 5) (cons (list 4 y) 5)))
@@ -136,4 +156,42 @@
   (check-equal?
     (run* (q) with-constraints (rembero 'a '(a b c) '(a b c)))
     '())
+  (check-equal?
+    (run* (x y) with-constraints (ino (range 3) x y) (all-diffo (list x y)))
+    '((0 1) (0 2) (1 0) (1 2) (2 0) (2 1)))
+  (check-equal?
+    (run* (w x y z) with-constraints (ino (range 3) w x y z)
+          (all-diffo (list w x y z)))
+    '())
+  (lets
+    ;   S E N D
+    ; + M O R E
+    ; ---------
+    ; M O N E Y
+    add-digitso = (fn (augend addend carry-in carry-out digit)
+      (exist (partial-sum sum)
+        (+o augend addend partial-sum)
+        (+o partial-sum carry-in sum)
+        (conde
+          ((<o 9 sum) (== carry-out 1) (+o digit 10 sum))
+          ((<=o sum 9) (== carry-out 0) (== digit sum)))
+        (ino (range 19) partial-sum)
+        (ino (range 20) sum)
+        ))
+    send-more-moneyo = (fn (letters)
+      (exist (s e n d m o r y carry0 carry1 carry2)
+        (== letters (list s e n d m o r y))
+        (all-diffo letters)
+        (add-digitso s m carry2 m o)
+        (add-digitso e o carry1 carry2 n)
+        (add-digitso n r carry0 carry1 e)
+        (add-digitso d e 0 carry0 y)
+        (ino (range 1 10) s m)
+        (ino (range 10) e n d o r y)
+        (ino (range 2) carry0 carry1 carry2)
+        ))
+    (check-equal?
+      (run* (a b c) with-constraints
+            (send-more-moneyo (list a 5 6 b 1 0 c 2)))
+      '((9 7 8))))
   )
