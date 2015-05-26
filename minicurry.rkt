f287c5069b462930867bb33e725f1d616a7a8065
gregr <greg.weiqi@gmail.com>
2015-05-26 17:11:47 -0400

    Redefine minicurry in terms of microkanren

--- minicurry.rkt
+++ minicurry.rkt
@@ -1,44 +1,46 @@
 #lang racket/base
 (provide
+  denote
+  denote-eval
+  env-empty
+  senv-new
   )
 
 (require
   "dict.rkt"
   "list.rkt"
   "maybe.rkt"
+  "microkanren.rkt"
   "sugar.rkt"
   racket/dict
   racket/function
-  racket/match
+  (except-in racket/match ==)
   )
 
 (module+ test
   (require
+    (only-in "minikanren.rkt" run run*)
     rackunit
     ))
 
 ; TODO:
 ; syntactic sugar for if, tag-case, general match
-; logic terms
-  ; logic vars
-; denote : syntax -> env -> logic-result -> st -> [st]
-;   or?
-; denote : syntax -> env -> st -> [st]
 
 (define ((app1 arg) f) (f arg))
 (define (atom? x)
-  (ormap (lambda (p) (p x)) (list symbol? boolean? number? string? char?)))
+  (ormap (lambda (p) (p x))
+         (list muk-var? symbol? boolean? number? string? char?)))
 
+(define ((muk-value val) st) (muk-unit st val))
 (define val-unit '())
 (define val-nil '())
 (define val-cons cons)
-(define denote-unit (const val-unit))
+(define denote-unit (const (muk-value val-unit)))
 (define (build-cons fst snd) (val-cons (build-datum fst) (build-datum snd)))
 (define (build-datum stx) (match stx
                             ((? atom?) stx)
                             ('() val-nil)
                             ((cons fst snd) (build-cons fst snd))))
-(define pretty-datum identity)
 
 (define (env-get env ident) (dict-get env ident))
 (define (env-add env ident val) (dict-set env ident val))
@@ -47,16 +49,30 @@
                                       (env-add env param #f)))
 (define env-empty (hash))
 
+(define (((eval-goal-cont cont) value-goal) st)
+  (match value-goal
+    ((muk-success value) ((cont value) st))
+    ((muk-conj-seq c0 c1)
+     (list (list st (muk-conj-seq c0 ((eval-goal-cont cont) c1)))))
+    ((muk-conj-conc _ _) (error "misplaced concurrent conjunction"))
+    (_ (forl (list st comp) <- (value-goal st)
+             (list st ((eval-goal-cont cont) comp))))))
+(define ((eval-application gargs) proc)
+  (match gargs
+    ('() (muk-value proc))
+    ((cons garg gargs)
+     ((eval-goal-cont
+        (compose1 (eval-goal-cont (eval-application gargs)) proc)) garg))))
 (def (denote-application senv head tail)
   dproc = (denote-with senv head)
   dargs = (map (curry denote-with senv) tail)
   (lambda (env)
-    (forf proc = (dproc env)
-          arg <- (map (app1 env) dargs)
-          (proc arg))))
+    (lets gproc = (dproc env)
+          gargs = (map (app1 env) dargs)
+          ((eval-goal-cont (eval-application gargs)) gproc))))
 (define (denote-quote _ tail)
   (match tail
-    ((list single) (const (build-datum single)))
+    ((list single) (const (muk-value (build-datum single))))
     (_ (error (format "invalid quote: ~a" `(quote . ,tail))))))
 
 (define (denote-lam senv tail)
@@ -64,7 +80,8 @@
     ((list (? list? (? (fn (ps) (andmap symbol? ps)) params)) body)
      (foldr (lambda (param body)
               (lambda (env)
-                (lambda (arg) (body (env-add env param arg)))))
+                (muk-value
+                  (lambda (arg) (body (env-add env param (muk-value arg)))))))
             (denote-with (env-extend senv params) body)
             params))
     (_ (error (format "invalid lam: ~a" `(lam . ,tail))))))
@@ -96,27 +113,37 @@
         (dbody env)))
     (err)))
 
+(define (eval-all-args gargs (rargs '()))
+  (match gargs
+    ('() (muk-value (reverse rargs)))
+    ((cons garg gargs)
+     ((eval-goal-cont
+        (lambda (arg) (eval-all-args gargs (list* arg rargs)))) garg))))
 (define ((denote-special-proc proc-name nargs proc) senv tail)
   (match tail
     ((? list? (? (compose1 (curry = nargs) length)))
      (lets args = (map (curry denote-with senv) tail)
-           (lambda (env) (apply proc (map (app1 env) args)))))
+           (fn (env)
+             gargs = (map (app1 env) args)
+             ((eval-goal-cont (lambda (args) (apply proc args)))
+              (eval-all-args gargs)))))
     (_ (error (format "'~a' expects ~a argument(s): ~a"
                       proc-name nargs `(,proc-name . ,tail))))))
+; TODO: handle logic vars
 (define denote-type
   (denote-special-proc 'type 1
     (lambda (arg)
-      (cond ((null? arg) 'nil)
-            ((pair? arg) 'pair)
-            ((atom? arg) 'atom)
-            ((procedure? arg) 'procedure)
-            (else (error (format "cannot determine type of: ~a" arg)))))))
-(define denote-pair (denote-special-proc 'pair 2 cons))
-(define denote-head (denote-special-proc 'head 1 car))
-(define denote-tail (denote-special-proc 'tail 1 cdr))
+      (muk-value
+        (cond ((null? arg) 'nil)
+              ((pair? arg) 'pair)
+              ((atom? arg) 'atom)
+              ((procedure? arg) 'procedure)
+              (else (error (format "cannot determine type of: ~a" arg))))))))
+(define denote-pair (denote-special-proc 'pair 2 (compose1 muk-value cons)))
+(define denote-head (denote-special-proc 'head 1 (compose1 muk-value car)))
+(define denote-tail (denote-special-proc 'tail 1 (compose1 muk-value cdr)))
 
-(define ((eval-seq dc0 dtail) env)
-  (if (dc0 env) (dtail env) (error "sequential conjunction failure")))
+(define ((eval-seq dc0 dtail) env) (conj-seq (dc0 env) (dtail env)))
 (define ((denote-conj+seq error-msg) senv tail)
   (match tail
     ((? list? (? (compose1 (curry < 0) length)) tail)
@@ -126,22 +153,26 @@
            (eval-seq dconj dbody)))
     (_ (error error-msg))))
 
-(define denote-== (denote-special-proc '== 2 equal?))
+(define denote-== (denote-special-proc '== 2 ==))
 (define (denote-exist senv tail)
   (match tail
     ((cons (? list? (? (fn (ps) (andmap symbol? ps)) params)) body)
      (lets senv = (env-extend senv params)
            error-msg = (format "invalid 'exist': ~a" `(exist ,params . ,body))
            dbody = ((denote-conj+seq error-msg) senv body)
-           (lambda (env) (dbody (forf env = env
-                                      param <- params
-                                      (env-add env param param))))))
+           (forf dbody = dbody
+                 param <- params
+                 (lambda (env)
+                   (call/var (lambda (x0)
+                               (dbody (env-add env param (muk-value x0)))))))))
     (_ (format "invalid 'exist': ~a" `(exist . ,tail)))))
 (define (denote-conj senv tail)
   (if (not (list? tail))
     (error (format "invalid conjunction: ~a" `(conj . ,tail)))
     (lets args = (map (curry denote-with senv) tail)
-          (lambda (env) (andmap (app1 env) args)))))
+          (fn (env)
+            gargs = (map (app1 env) args)
+            (foldr conj muk-unit gargs)))))
 (define (denote-seq senv tail)
   (match tail
     ((list body) (denote-with senv body))
@@ -152,13 +183,13 @@
     (_ (error (format "invalid sequential conjunction: ~a" `(seq . ,tail))))))
 (define (denote-disj senv tail)
   (match tail
-    ('() (lambda (_) (error "disjunction failure")))
+    ('() (lambda (_) (const muk-mzero)))
     ((cons (? list? (? (compose1 (curry < 0) length)) head) tail)
      (lets (list conditions body) = (list-init+last head)
            dconj = (denote-conj senv conditions)
            dbody = (denote-with senv body)
            dtail = (denote-disj senv tail)
-           (lambda (env) (if (dconj env) (dbody env) (dtail env)))))
+           (lambda (env) (disj ((eval-seq dconj dbody) env) (dtail env)))))
     (_ (error (format "invalid disjunction: ~a" `(disj . ,tail))))))
 
 (define senv-new (hash
@@ -182,11 +213,10 @@
     ((just special?)
      (if special?
        (error (format "invalid use of special identifier: ~a" ident))
-       (lambda (env)
-         (match (just-x (env-get env ident))
-           ((? box? val) (unbox val))
-           (val val)))))))
-(define (denote-atom atom) (const atom))
+       (lambda (env) (match (just-x (env-get env ident))
+                       ((? box? val) (unbox val))
+                       (val val)))))))
+(define (denote-atom atom) (const (muk-value atom)))
 
 (define (denote-with senv stx)
   (match stx
@@ -203,45 +233,54 @@
 
 (module+ test
   (check-equal?
-    (pretty-datum (denote-eval '(quote (a b))))
-    '(a b))
+    (run* (q) (denote-eval `(== ,q (quote (a b)))))
+    '(((a b))))
   (check-equal?
-    (pretty-datum (denote-eval '((lam (x y) x) 5 (quote (a b c)))))
-    5)
+    (run* (q) (denote-eval `(== ,q ((lam (x y) x) 5 (quote (a b c))))))
+    '((5)))
   (check-equal?
-    (pretty-datum
-      (denote-eval '((lam (rec val) (type 'sym)) () 5)))
-    'atom)
+    (run* (q r)
+      (denote-eval `(== ,q ((lam (x y) (seq (== ,r y) x)) 5 (quote (a b c))))))
+    '((5 (a b c))))
   (check-equal?
-    (pretty-datum
-      (denote-eval '((lam (rec val) (pair val rec)) () 5)))
-    '(5))
+    (run* (q) (denote-eval `(== ,q ((lam (rec val) (pair val rec)) () 6))))
+    '(((6))))
   (check-equal?
-    (pretty-datum
-      (denote-eval '((lam (rec val) (head (pair val rec))) () 4)))
-    4)
+    (run* (q)
+      (denote-eval `(== ,q ((lam (rec val) (head (pair val rec))) () 4))))
+    '((4)))
   (check-equal?
-    (pretty-datum
-      (denote-eval '((lam (rec val) (tail (pair val rec))) () 4)))
-    '())
+    (run* (q)
+      (denote-eval `(== ,q ((lam (rec val) (tail (pair val rec))) () 4))))
+    '((())))
   (check-equal?
-    (pretty-datum
-      (denote-eval '((exist (a b) (== #t #t) (pair a (pair b ()))))))
-    '(a b))
-  (check-equal?
-    (pretty-datum
+    (run* (q)
       (denote-eval
-        '(letr
-           ((list-case xs nil-case pair-case)
+        `((exist (a b) (== 1 a) (== 2 b) (== ,q (pair a (pair b ())))))))
+    '(((1 2))))
+  (check-equal?
+    (run* (q)
+      (denote-eval
+        `(== ,q
+           (letr ((test x y) x)
+                 (test 3 4)))))
+    '((3)))
+  (check-equal?
+    (run* (q)
+      (denote-eval
+        `(== ,q
+           (letr
+             ((list-case xs nil-case pair-case)
               (disj ((== 'nil (type xs)) (nil-case '()))
                     ((== 'pair (type xs)) (pair-case (head xs) (tail xs)))))
-           ((even? xs) (list-case xs
-                                  (lam (_) #t)
-                                  (lam (hd tl) (odd? tl))))
-           ((odd? xs) (list-case xs
-                                 (lam (_) #f)
-                                 (lam (hd tl) (even? tl))))
-           (pair (pair 'is-even (pair (even? '(a b c)) ()))
-                 (pair (pair 'is-odd (pair (odd? '(a b c)) ())) ())))))
-    '((is-even #f) (is-odd #t)))
+             ((even? xs) (list-case xs
+                                    (lam (_) #t)
+                                    (lam (hd tl) (odd? tl))))
+             ((odd? xs) (list-case xs
+                                   (lam (_) #f)
+                                   (lam (hd tl) (even? tl))))
+             (pair (pair 'is-even (pair (even? '(a b c)) ()))
+                   (pair (pair 'is-odd (pair (odd? '(a b c)) ())) ()))))))
+    '((((is-even #f) (is-odd #t)))))
+  ; TODO: append, last in terms of append
   )
