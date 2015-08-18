9684615e4af397a17ad8e65a8a726c8558fa5752
gregr <greg.weiqi@gmail.com>
2015-08-18 18:46:48 -0400

    Define typenames-free, typenames-used

--- syntax.rkt
+++ syntax.rkt
@@ -2,12 +2,23 @@
 (provide
   identifier-concat
   identifier-with-?
+  typenames-free
+  typenames-used
   )
 
 (require
+  "idset.rkt"
+  "typed/syntax.rkt"
   racket/list
+  racket/match
   racket/string
   )
+
+(module+ test
+  (require
+    rackunit
+    racket/set
+    ))
 
 (define (identifier-concat stx idents)
   (datum->syntax
@@ -16,3 +27,35 @@
       (string-append* (map symbol->string (syntax->datum idents))))))
 
 (define (identifier-with-? ident) (identifier-concat ident #`(#,ident ?)))
+
+(define (typenames-free type)
+  (match (syntax->list type)
+    ((cons ctor args)
+     (cond
+       ((identifier=All? ctor)
+        (match-let (((list params body) args))
+          (idset-subtract (typenames-free body)
+                          (list->idset (syntax->list params)))))
+       ((identifier=->*? ctor)
+        (match-let (((list mandatory optional result) args))
+          (typenames-free
+            #`(() #,result
+                  #,@(append* (map syntax->list (list mandatory optional)))))))
+       (else (idset-union* (map typenames-free args)))))
+    (#f (idset-single type))))
+
+(define (typenames-used params type)
+  (idset-intersect (list->idset params) (typenames-free type)))
+
+(module+ test
+  (check-equal?
+    (list->set (map syntax->datum (idset->list
+      (typenames-free
+        #'(All (a b) (-> (->* (a b) (a) Number) c (Listof String) Number))))))
+    (list->set '(Number String c)))
+  (check-equal?
+    (map syntax->datum (idset->list
+      (typenames-used
+        (syntax->list #'(a b c))
+        #'(All (a b) (-> (->* (a b) (a) Number) c (Listof String) Number)))))
+    '(c)))
