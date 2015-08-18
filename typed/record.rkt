70197149add40c07b7df35c0d524c26afc3256c7
gregr <greg.weiqi@gmail.com>
2015-08-18 18:50:06 -0400

    Define typed 'records'

--- typed/record.rkt
+++ typed/record.rkt
@@ -1,12 +1,17 @@
 #lang typed/racket/base
 (provide
   record
+  records
   record-struct
   )
 
 (require
   (for-syntax
+    "../idset.rkt"
+    "../sugar.rkt"
+    "../syntax.rkt"
     racket/base
+    racket/function
     ))
 
 (define-syntax (record-struct stx)
@@ -21,3 +26,18 @@
     ((_ (name type-params ...) field ...)
      (record-struct (name type-params ...) (field ...)))
     ((_ name field ...) (record (name) field ...))))
+
+(define-syntax (records stx)
+  (syntax-case stx (:)
+    ((_ (name type-params ...) (rec-name (field-name : field-type) ...) ...)
+     (let* ((params (syntax->list #'(type-params ...)))
+            (useds (map (compose1 idset->list (curry typenames-used params))
+                        (syntax->list #'((() field-type ...) ...))))
+            (templates
+              (map syntax->list (syntax->list
+                #'((rec-name ((field-name : field-type) ...)) ...))))
+            (recs (forl
+                    used <- useds
+                    (list rname rfields) <- templates
+                    #`(record (#,rname #,@used) #,@rfields))))
+       #`(begin #,@recs)))))
