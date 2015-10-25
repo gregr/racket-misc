58b5bc7ebb110c0e8dbd4043e83466e13393bad1
gregr <greg.weiqi@gmail.com>
2015-10-25 15:45:58 -0400

    Define default methods and object-understands?

--- oop.rkt
+++ oop.rkt
@@ -6,6 +6,7 @@
   object-empty
   object-method-names
   object-new
+  object-understands?
   )
 
 (require
@@ -13,21 +14,32 @@
   "record.rkt"
   "sugar.rkt"
   racket/dict
+  racket/function
   )
 
-(record object methods)
-(define object-empty (object hash-empty))
-(def (object-inherit (object methods) (object prototype-methods))
+(define ((unknown-method method) self . args)
+  (error (format "accessed unknown method ~a of ~v with args: ~v"
+                 method self args)))
+(define unknown-understands? (const #f))
+(record object methods default-method default-understands?)
+(define object-empty (object hash-empty unknown-method unknown-understands?))
+(def (object-inherit (object methods dm du?) (object prototype-methods _ _))
   prototype-methods =
   (if (hash? prototype-methods)
     prototype-methods (dict-join hash-empty prototype-methods))
-  (object (dict-join prototype-methods methods)))
-(define (object-new prototypes (methods hash-empty))
-  (object-inherit (object methods)
+  (object (dict-join prototype-methods methods) dm du?))
+(define (object-new prototypes (methods hash-empty)
+                    (default-method unknown-method)
+                    (default-understands? unknown-understands?))
+  (object-inherit (object methods default-method default-understands?)
                   (foldl object-inherit object-empty prototypes)))
-(def (object-method-names (object methods)) (dict-keys methods))
+(def (object-method-names (object methods _ _)) (dict-keys methods))
+(def (object-understands? (object methods _ default-understands?) method)
+  (or (default-understands? method) (dict-has-key? methods method)))
 (define (o@ obj method . args)
-  (apply (hash-ref (object-methods obj) method) obj args))
+  (lets (object methods default-method _) = obj
+        (apply (dict-ref methods method (thunk (default-method method)))
+               obj args)))
 
 (define-syntax method-table
   (syntax-rules ()
@@ -66,6 +78,11 @@
     obj1 = (object-new `(,obj0)
              (method-table self
                (three+ args (+ 100 (apply o@ obj0 'three+ args)))))
+    default = (lambda (method)
+                (lambda (self . args)
+                  (format "received ~a with args: ~v" method args)))
+    obj2 = (object-new (list obj1) (method-table) default (const #t))
+    obj3 = (object-new (list obj2))
     (begin
       (check-equal? (method-table) hash-empty)
       (check-equal?
@@ -75,6 +92,20 @@
         (o@ obj1 'format "~a; ~a" 1 2)
         "welcome!; 108")
       (check-equal?
+        (o@ obj2 'format "~a; ~a" 1 2)
+        "welcome!; 108")
+      (check-equal?
+        (o@ obj2 'format2 "~a; ~a" 1 2)
+        "received format2 with args: '(\"~a; ~a\" 1 2)")
+      (check-equal?
         (list->set (object-method-names obj1))
         (set 'one 'three+ 'five+ 'format))
+      (check-equal?
+        (list->set (object-method-names obj2))
+        (list->set (object-method-names obj1)))
+      (check-true (object-understands? obj1 'format))
+      (check-true (object-understands? obj2 'format))
+      (check-false (object-understands? obj1 'format2))
+      (check-true (object-understands? obj2 'format2))
+      (check-false (object-understands? obj3 'format2))
       )))
