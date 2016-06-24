bfcc71068a278ddb2cf85d689cf015633e697f01
gregr <greg.weiqi@gmail.com>
2016-06-24 16:02:20 -0400

    Benchmark run-da-dls on logo

--- minikanren-numbers.rkt
+++ minikanren-numbers.rkt
@@ -3,6 +3,7 @@
 
   )
 (require
+  "dakanren.rkt"
   "minikanren.rkt"
   )
 
@@ -446,8 +447,9 @@
   ;((1 1 1) (0 1) (1 1 0 0 1)))
 
   ; depth=20 gives these 9 answers in 4.25s
-  (check-equal? (list->set (run-dls 9 (1 (lambda (d) (+ 10 d)) #f)
-                                    (b q r) (logo '(0 0 1 0 0 0 1) b q r) (>1o q)))
+  (check-equal? (list->set (run-da-dls
+                             9 (20 (lambda (d) (+ 10 d)) #f)
+                             (b q r) (logo '(0 0 1 0 0 0 1) b q r) (>1o q)))
                 (list->set
                   '((() (_.0 _.1 . _.2) (0 0 1 0 0 0 1))
                     ((1) (_.0 _.1 . _.2) (1 1 0 0 0 0 1))
