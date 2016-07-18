20d154baa29290ba5d5f1865fefef6aeb0289ebe
gregr <greg.weiqi@gmail.com>
2016-07-18 14:32:44 -0400

    Test quine generation performance

--- okanren-benchmarks.rkt
+++ okanren-benchmarks.rkt
@@ -387,3 +387,121 @@
       ;(((0 1 1) (0 1) (0 0 0 0 0 1)))
       ;(((1 1 1) (0 1) (1 1 0 0 1)))))
   )
+
+(kanren
+  (define eval-expo
+    (lambda (exp env val)
+      (conde
+        ((fresh (v)
+           (== `(quote ,v) exp)
+           (not-in-envo 'quote env)
+           (absento 'closure v)
+           (== v val)))
+        ((fresh (a*)
+           (== `(list . ,a*) exp)
+           (not-in-envo 'list env)
+           (absento 'closure a*)
+           (proper-listo a* env val)))
+        ((symbolo exp) (lookupo exp env val))
+        ((fresh (rator rand x body env^ a)
+           (== `(,rator ,rand) exp)
+           (eval-expo rator env `(closure ,x ,body ,env^))
+           (eval-expo rand env a)
+           (eval-expo body `((,x . ,a) . ,env^) val)))
+        ((fresh (x body)
+           (== `(lambda (,x) ,body) exp)
+           (symbolo x)
+           (not-in-envo 'lambda env)
+           (== `(closure ,x ,body ,env) val))))))
+
+  (define lookupo
+    (lambda (x env t)
+      (fresh (y v rest)
+        (== `((,y . ,v) . ,rest) env)
+        (conde
+          ((== y x) (== v t))
+          ((=/= y x) (lookupo x rest t))))))
+
+  (define not-in-envo
+    (lambda (x env)
+      (conde
+        ((== '() env))
+        ((fresh (y v rest)
+           (== `((,y . ,v) . ,rest) env)
+           (=/= y x)
+           (not-in-envo x rest))))))
+
+  (define proper-listo
+    (lambda (exp env val)
+      (conde
+        ((== '() exp)
+         (== '() val))
+        ((fresh (a d v-a v-d)
+           (== `(,a . ,d) exp)
+           (== `(,v-a . ,v-d) val)
+           (eval-expo a env v-a)
+           (proper-listo d env v-d)))))))
+
+(define quinec
+  '((lambda (_.0)
+      (list _.0 (list (quote quote) _.0)))
+    (quote
+      (lambda (_.0)
+        (list _.0 (list (quote quote) _.0))))))
+
+(define twine1
+  '((lambda (_.0) (list 'quote (list _.0 (list 'quote _.0))))
+    '(lambda (_.0) (list 'quote (list _.0 (list 'quote _.0))))))
+(define twine0 (list 'quote twine1))
+
+(define thrine2
+  '((lambda (_.0) (list 'quote (list 'quote (list _.0 (list 'quote _.0)))))
+    '(lambda (_.0) (list 'quote (list 'quote (list _.0 (list 'quote _.0)))))))
+(define thrine1 (list 'quote thrine2))
+(define thrine0 (list 'quote thrine1))
+
+(module+ test
+  (mk-test "quote"
+    (run* (q) (eval-expo '(quote 5) '() q))
+    '(((5))))
+
+  (mk-test "lambda"
+    (run* (q) (eval-expo '(lambda (x) x) '() q))
+    '((((closure x x ())))))
+
+  (mk-test "quine parts"
+    (run 1 (q) (eval-expo
+                 `((lambda (x) (list x ,q))
+                   (quote
+                     (lambda (x) (list x ,q))))
+                 '()
+                 `((lambda (x) (list x ,q))
+                   (quote
+                     (lambda (x) (list x ,q))))))
+    '((((list (quote quote) x)))))
+
+  (mk-test "quine more parts"
+    (run 1 (q) (eval-expo
+                 `((lambda (x) ,q)
+                   (quote (lambda (x) ,q)))
+                 '()
+                 `((lambda (x) ,q)
+                   (quote (lambda (x) ,q)))))
+    '((((list x (list (quote quote) x))))))
+
+  (mk-test-time "quine full"
+    (run 1 (q) (eval-expo q '() q))
+    `(((,quinec) (sym _.0) (absento (_.0 closure)) (=/=* ((_.0 . quote)) ((_.0 . list))))))
+
+  ; aim for less than a second
+  ;(mk-test-time "twine"
+    ;(run 1 (p q) (=/= p q) (eval-expo p '() q) (eval-expo q '() p))
+    ;`(((,twine0 ,twine1)
+       ;(sym _.0) (absento (_.0 closure)) (=/=* ((_.0 . quote)) ((_.0 . list))))))
+
+  ; aim for 3 seconds
+  ;(mk-test-time "thrine"
+    ;(run 1 (p q r) (=/= p q) (=/= q r) (=/= r p)
+      ;(eval-expo p '() q) (eval-expo q '() r) (eval-expo r '() p))
+    ;`((,thrine0 ,thrine1 ,thrine2)))
+  )
